51f8354f9b1e59c0e3167a6757cf69986b7399d9
VarshiniShanmugam2416 <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2017-09-03 20:25:31 +0530

    Update varsh8.c

--- varsh8.c
+++ varsh8.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    long long n;
-    int count = 0;
-
-    printf("Enter an integer: ");
-    scanf("%lld", &n);
-
-    while(n != 0)
+    int n, count=0;
+    scanf("%d", &n);
+    if(n>0)
     {
-        // n = n/10
         n /= 10;
         ++count;
     }
