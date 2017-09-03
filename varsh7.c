445bdbc422cc273e8ad7ca68a116a122c0d3c539
VarshiniShanmugam2416 <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2017-09-03 20:32:46 +0530

    Update varsh7.c

--- varsh7.c
+++ varsh7.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 int main()
 {
-  int n,i,sum=0;
-  scanf("%d",&n);
-  for(i=0;i<=n;i++)
-    {
-    sum=sum+i;
-    }
+  int n1,n2,n3,n4,n5,sum=0;
+  scanf("%d%d%d%d%d",&n1, &n2, &n3, &n4, &n5);
+    sum=n1+n2+n3+n4+n5;
 printf("sum=%d",sum);
 return 0;
 }
