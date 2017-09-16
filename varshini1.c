b2de94425960a3f864c2bd3d661c177ad5d88de7
VarshiniShanmugam2416 <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2017-09-16 10:28:46 +0530

    Create varshini1.c

---
+++ varshini1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+scanf("%d %d",&a,&b);
+if(a<b)
+{
+for(i=a;i<=b;i++)
+{
+if(i%2==1)
+printf("%d is odd",i);
+}
+}
+if(b<a)
+{
+for(i=;i<=a;i++)
+{
+if(i%2==1)
+printf("%d is odd",i);
+}
+}
+}
