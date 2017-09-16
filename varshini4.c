7649cb6db81336084a51e9dad6f085e791423780
VarshiniShanmugam2416 <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2017-09-16 14:40:38 +0530

    Create varshini4.c

---
+++ varshini4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  int a=150,b,c=160,sum,rem,i,temp,temp1,n;
+  scanf("%d",&a,&c);
+  for(i=a+1;i>=c;i++)
+  {
+    temp1=i;
+    temp=i;
+  }
+  while(temp!=0)
+  {
+    temp=temp/10;
+    n=++i;
+  }
+  if(temp1!=0)
+  {
+     sum=a%10;
+     rem=sum*sum*sum;
+     b=a/10;
+  }
+if(a==i)
+   {
+    printf("armstrong");
+n=0;
+rem=0;
+}
+return 0;
+}
