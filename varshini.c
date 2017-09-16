f20bd471bf8725784a111e809c7ca5fb870ba516
VarshiniShanmugam2416 <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2017-09-16 10:24:55 +0530

    Create varshini.c

---
+++ varshini.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,i,count=0;
+scanf("%d",&a);
+for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+      {
+        count=count+1;
+       }
+  }
+if(count==2)
+printf("prime number");
+else
+printf("not prime");
+}
