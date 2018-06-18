925580bdfe5e920975a597384a39b1067523f172
Varshini_Shanmugam <31567390+VarshiniShanmugam2416@users.noreply.github.com>
2018-06-18 18:23:59 +0530

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,12 +1,16 @@
-#include<conio.h>
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+int main(void) 
 {
-int a,ans=0,j=1;
+int a,ans=0,j=1,i;
+printf("Enter the number: ");
 scanf("%d",&a);
 for(i=0;i<5;i++)
 {
 ans=a*j;
 j++;
-printf("%d",ans);
+printf("%d ",ans);
+}
+
+	return 0;
 }
